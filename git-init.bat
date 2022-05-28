6536626b11dd2b6a89b906ad5c4cc39120920204
Het Patel <hetpatelofficial@gmail.com>
2022-05-28 06:29:31 +0530

    initial commit

---
+++ git-init.bat
@@ -0,0 +1,5 @@
+git init -b main
+git add . && git commit -m "initial commit"
+git remote add origin  https://github.com/hetpatelofficial/het-portfolio-website.git 
+git remote -v
+git push origin main
